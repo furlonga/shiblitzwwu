4b5c5a10c26d92ddf8229879392e057b58cd9ca6
Trevor <trevoryokoyama@gmail.com>
2020-02-09 15:42:14 -0800

    Integrated unity into Android Studio

--- app/src/main/java/wwu/edu/csci412/cp2/MainActivity.java
+++ app/src/main/java/wwu/edu/csci412/cp2/MainActivity.java
@@ -2,12 +2,17 @@ package wwu.edu.csci412.cp2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import com.shiblitz.unity.UnityPlayerActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Intent intent = new Intent(this, UnityPlayerActivity.class);
+        intent.putExtra("arguments", "data from android");
+        startActivity(intent);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
