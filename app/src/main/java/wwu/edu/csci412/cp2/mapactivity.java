454355a4c65ee5ce41ea3a775d0fd095bed938f6
albert.joe.furlong@gmail.com <albert.joe.furlong@gmail.com>
2020-02-09 13:41:10 -0800

    adding map activity

---
+++ app/src/main/java/wwu/edu/csci412/cp2/mapactivity.java
@@ -0,0 +1,14 @@
+package wwu.edu.csci412.cp2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class mapactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mapactivity);
+    }
+}
