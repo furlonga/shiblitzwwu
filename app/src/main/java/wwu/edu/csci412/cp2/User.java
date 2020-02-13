1780ef8267dec107d3ba576faf2e6131a6bd5fff
mymddoan <doanm3@wwu.edu>
2020-02-13 15:15:31 -0800

    a

---
+++ app/src/main/java/wwu/edu/csci412/cp2/User.java
@@ -0,0 +1,42 @@
+package wwu.edu.csci412.cp2;
+
+public class User {
+    private String email;
+    private String name;
+
+    private int xp;
+    private int hp;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getXp() {
+        return xp;
+    }
+
+    public void setXp(int xp) {
+        this.xp = xp;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+}
